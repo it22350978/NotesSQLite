ee808895f5b1aff8824c55996b6059b96831d6b0
it22350978 <134755850+it22350978@users.noreply.github.com>
2024-05-12 10:33:18 +0530

    final commit

---
+++ app/src/main/java/com/example/notessqlite/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.notessqlite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.notessqlite.databinding.ActivityMainBinding
+import android.content.Intent
+import androidx.recyclerview.widget.LinearLayoutManager
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db: NotesDatabaseHelper
+    private lateinit var notesAdapter: NotesAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
+        val notesList: List<Note> = db.getAllNotes()
+        notesAdapter = NotesAdapter(notesList, this)
+
+        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter = notesAdapter
+
+        binding.addButton.setOnClickListener {
+            val intent = Intent(this,AddNoteActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        notesAdapter.refreshData(db.getAllNotes())
+    }
+}
